a3e46cf310201cd89a73e3a9f2c888f485a7767a
Makita0411 <ee227083@meiji.ac.jp>
2025-10-10 19:38:08 +0900

    edit setting

--- usr/local/bin/setting.py
+++ usr/local/bin/setting.py
@@ -26,7 +26,7 @@ def update_application():
                 f.write(chunk)
         
         # インストールコマンドをpkexecに変更 (パスワード入力ダイアログが表示される)
-        install_command = ["pkexec", "apt", "install", "-y", temp_file_path]
+        install_command = ["pkexec", "bash", "-c", f"cd /tmp && apt install -y ./{deb_filename}"]
         
         # subprocess.run を使用してコマンドを実行
         subprocess.run(install_command, check=True)
